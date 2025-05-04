0fbbe2a7a048834edb4d7528cbe33554748287a2
Naominm <naomimbugua536@gmail.com>
2025-05-04 16:42:07 +0300

    Initial Project reset

---
+++ src/components/card.tsx
@@ -0,0 +1,15 @@
+import {Paper,Box,Typography} from "@mui/material";
+
+function Card() {
+    return ( 
+    <Box>
+        <Paper>
+        <Typography variant="h6">Hello the payment component will go here</Typography>
+        </Paper>
+    </Box>
+    );
+}
+
+export default Card;
+<>
+</>
