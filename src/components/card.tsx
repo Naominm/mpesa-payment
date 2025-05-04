c2ceac9a72e183aac17dbc6584d4e9dbd037d5e5
Naominm <naomimbugua536@gmail.com>
2025-05-04 17:16:51 +0300

    Add the mpesa logo and center it

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,10 +1,15 @@
 import {Paper,Box,Typography} from "@mui/material";
+import mpesaLogo from "../assets/mpesa.png"
 
 function Card() {
     return ( 
     <Box component="div" sx={{display:"flex",alignItems:'center',justifyContent:"center", backgroundColor:"#F6F8FA", height:"100vh"}}>
-        <Paper sx={{height:"30rem"}}>
-        <Typography variant="h6">Hello the payment component will go here</Typography>
+        <Paper sx={{height:"30rem", width:"30%"}}>
+       <Box component="div" sx={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+       <Box component="div" sx={{width:"12rem",height:"5rem", objectFit:"cover"}}> <img src={mpesaLogo} alt=""  style={{ width: "100%", height: "100%", objectFit: "cover" }}/> </Box>
+       <Box></Box>
+       </Box>
+       
         </Paper>
     </Box>
     );
