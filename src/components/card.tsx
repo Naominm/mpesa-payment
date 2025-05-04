91696db4534d4f9f0404685c1351633feae87307
Naominm <naomimbugua536@gmail.com>
2025-05-04 16:48:05 +0300

    Design the card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,8 +2,8 @@ import {Paper,Box,Typography} from "@mui/material";
 
 function Card() {
     return ( 
-    <Box>
-        <Paper>
+    <Box component="div" sx={{display:"flex",alignItems:'center',justifyContent:"center", backgroundColor:"#F6F8FA", height:"100vh"}}>
+        <Paper sx={{height:"30rem"}}>
         <Typography variant="h6">Hello the payment component will go here</Typography>
         </Paper>
     </Box>
